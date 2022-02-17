ee3346cb77f980d8812e61160a1e272090224bda
dwolffram <25206417+dwolffram@users.noreply.github.com>
2022-02-17 16:47:46 +0100

    adjusted path

--- plot_murphy.R
+++ plot_murphy.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-
 Sys.setlocale("LC_ALL", "C")
 
 elementary_quantile_score <- function(y_true, y_pred, theta, alpha){
@@ -13,8 +12,8 @@ get_thetas <- function(df, n=1001){
 }
 
 murphy_diagram <- function(df, models, target, 
-                                       quantiles = c(0.025, 0.1, 0.25, 0.5, 0.75, 0.9, 0.975, 0.01, 0.05, 0.15, 0.2, 
-                                                     0.3, 0.35, 0.4, 0.45, 0.55, 0.6, 0.65, 0.7, 0.8, 0.85, 0.95, 0.99)){
+                           quantiles = c(0.025, 0.1, 0.25, 0.5, 0.75, 0.9, 0.975, 0.01, 0.05, 0.15, 0.2, 
+                                         0.3, 0.35, 0.4, 0.45, 0.55, 0.6, 0.65, 0.7, 0.8, 0.85, 0.95, 0.99)){
   df <- df %>%
     filter(model %in% models,
            target == !!target,
@@ -33,7 +32,7 @@ murphy_diagram <- function(df, models, target,
   
   ggplot(df, aes(x=theta, y=mean_score, color=model)) +
     geom_line(size=0.5) +
-    facet_wrap("quantile", scales="free") +
+    facet_wrap("quantile", scales = "free") +
     # labs(title = paste("Murphy diagram:", target), color = "Model") +
     xlab(expression(paste("Parameter ", theta))) +
     ylab(NULL) +
@@ -46,14 +45,12 @@ murphy_diagram <- function(df, models, target,
 }
 
 
-eval_date <- '2022-01-03'
-
-df <- read_csv(paste0("evaluation/", eval_date, "_df_processed.csv"), col_types = cols()) %>%
+# Load data
+df <- read_csv("data/2022-01-03_df_processed.csv", col_types = cols()) %>%
   filter(location != "US")
 
+models <- c("KITmetricslab-select_ensemble", "COVIDhub-ensemble", "COVIDhub-baseline")
 
-models=c("KITmetricslab-select_ensemble", "COVIDhub-ensemble", "COVIDhub-baseline")
+murphy_diagram(df, models, "1 wk ahead inc death", quantiles = c(0.05, 0.5, 0.95))
 
-murphy_diagram(df, models, '1 wk ahead inc death', quantiles = c(0.05, 0.5, 0.95))
-
-ggsave("figures/murphy_states.pdf", width=210, height=100, unit="mm", device = "pdf", dpi=300)
+# ggsave("figures/murphy_states.pdf", width=210, height=100, unit="mm", device = "pdf", dpi=300)
