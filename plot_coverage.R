b22530c388ba398c757dc4381e2309573e375d03
dwolffram <25206417+dwolffram@users.noreply.github.com>
2022-02-21 14:40:03 +0100

    adjusted paths and added some variations

--- plot_coverage.R
+++ plot_coverage.R
@@ -34,12 +34,12 @@ plot_coverage <- function(df, date_column = target_end_date, B = 1000, differenc
   results <- coverage_df %>%
     group_by(model, quantile) %>%
     summarize(l_5 = quantile(l, 0.05),
-              # l_95 = quantile(l, 0.95),
+              l_95 = quantile(l, 0.95),
               l_25 = quantile(l, 0.25),
-              # l_75 = quantile(l, 0.75),
-              # u_5 = quantile(u, 0.05),
+              l_75 = quantile(l, 0.75),
+              u_5 = quantile(u, 0.05),
               u_95 = quantile(u, 0.95),
-              # u_25 = quantile(u, 0.25),
+              u_25 = quantile(u, 0.25),
               u_75 = quantile(u, 0.75), 
               .groups = "drop")
   
@@ -71,7 +71,10 @@ plot_coverage <- function(df, date_column = target_end_date, B = 1000, differenc
   }
   else{
     results_diff <- results %>% 
-      mutate_at(vars("l", "u", "l_5", "l_25", "u_75", "u_95"), list(~ . - quantile))
+      mutate_at(vars("l", "u", 
+                     "l_5", "l_25", "l_75", "l_95", 
+                     "u_5", "u_25","u_75", "u_95"), 
+                list(~ . - quantile))
     
     g <- ggplot(results_diff) +
       facet_wrap("model") +
@@ -96,7 +99,7 @@ plot_coverage <- function(df, date_column = target_end_date, B = 1000, differenc
 # Load data
 models <- c("KITmetricslab-select_ensemble", "COVIDhub-ensemble", "COVIDhub-baseline")
 
-df <- read_csv("data/2022-01-03_df_processed.csv", col_types = cols()) %>%
+df <- read_csv("data/2022-01-03_df_processed.csv.gz", col_types = cols()) %>%
   filter(location != "US")
 
 df <- df %>%
@@ -107,8 +110,89 @@ df <- df %>%
 # Plot coverage
 results <- plot_coverage(df, B = 1000)
 
+# ggsave("figures/coverage_national.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
 # ggsave("figures/coverage_states.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
 
 results_diff <- plot_coverage(df, B = 1000, difference = TRUE)
 
+# ggsave("figures/coverage_diff_national.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
 # ggsave("figures/coverage_diff_states.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
+
+
+
+### Using bars instead of bands
+
+ggplot(results_diff) +
+  facet_wrap("model", ncol=3) +
+  geom_segment(aes(x=0,xend=1,y=0,yend=0), size=0.2, linetype="solid", colour="grey70")+
+  geom_rect(aes(xmin=quantile-0.007, xmax=quantile+0.007, ymin=l, ymax=u), fill="deepskyblue4",alpha = 0.7) +
+  geom_errorbar(aes(x=quantile+0.0035, ymin=u_5, ymax=u_95), width=0.01, size=0.3,colour="red") +
+  geom_errorbar(aes(x=quantile-0.0035, ymin=l_5, ymax=l_95), width=0.01, size=0.3,colour="red") +
+  scale_x_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                     labels = function(x) ifelse(x == 0, "0", x)) +
+  scale_y_continuous(labels = function(y) ifelse(y == 0, "0", y)) +
+  xlab('Quantile') +
+  ylab(NULL) +
+  # labs(title="Coverage of death forecasts") +
+  theme_bw(base_size=11) +
+  theme(panel.grid.major = element_line(size = 0.05), 
+        panel.grid.minor = element_line(size = 0.05))
+
+ggplot(results_diff) +
+  facet_wrap("model", ncol=1) +
+  geom_segment(aes(x=0,xend=1,y=0,yend=0), size=0.2, linetype="solid", colour="grey70")+
+  geom_errorbar(aes(x=quantile+0.01, ymin=u_5, ymax=u_95), width=0.006, size=0.1,colour="firebrick1") +
+  geom_errorbar(aes(x=quantile-0.01, ymin=l_5, ymax=l_95), width=0.006, size=0.1,colour="firebrick1") +
+  geom_errorbar(aes(x=quantile, ymin = l, ymax = u), width = 0.0125, size = 0.3, colour = "black") +  
+  scale_x_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                     labels = function(x) ifelse(x == 0, "0", x)) +
+  scale_y_continuous(labels = function(y) ifelse(y == 0, "0", y)) +
+  xlab('Quantile') +
+  ylab(NULL) +
+  # labs(title="Coverage of death forecasts") +
+  theme_bw(base_size=11) +
+  theme(panel.grid.major = element_line(size = 0.05), 
+        panel.grid.minor = element_line(size = 0.05))
+
+# ggsave("figures/coverage_diff_3bars_long.pdf", width=200, height=200, unit="mm", device = "pdf", dpi=300)
+
+ggplot(results) +
+  facet_wrap("model", ncol=3) +
+  geom_segment(aes(x = 0, xend = 1, y = 0, yend = 1), size = 0.2, linetype = "solid", colour = "grey70")+
+  geom_errorbar(aes(x=quantile+0.01, ymin=u_5, ymax=u_95), width=0.006, size=0.1,colour="firebrick1") +
+  geom_errorbar(aes(x=quantile-0.01, ymin=l_5, ymax=l_95), width=0.006, size=0.1,colour="firebrick1") +
+  geom_errorbar(aes(x=quantile, ymin = l, ymax = u), width = 0.0125, size = 0.3, colour = "black") +  
+  scale_x_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                     labels = function(x) ifelse(x == 0, "0", x)) +
+  scale_y_continuous(labels = function(y) ifelse(y == 0, "0", y)) +
+  xlab('Quantile') +
+  ylab(NULL) +
+  # labs(title="Coverage of death forecasts") +
+  theme_bw(base_size = 11) +
+  theme(panel.grid.major = element_line(size = 0.05), 
+        panel.grid.minor = element_line(size = 0.05)) +
+  coord_fixed()
+
+# ggsave("figures/coverage_3bars.pdf", width=200, height=70, unit="mm", device = "pdf", dpi=300)
+
+
+
+### Different colors for both CIs
+
+ggplot(results_diff) +
+  facet_wrap("model") +
+  geom_hline(yintercept = 0, size = 0.3, linetype = "solid", color = "darkgray") +
+  geom_ribbon(aes(x = quantile, ymin = l_5, ymax = l_95), fill = "darkblue", alpha = 0.2) +
+  geom_ribbon(aes(x = quantile, ymin = u_5, ymax = u_95), fill = "darkred", alpha = 0.2) +
+  geom_errorbar(aes(x=quantile, ymin = l, ymax = u), width = 0.0125, size = 0.3, colour = "black") +
+  scale_x_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                     labels = function(x) ifelse(x == 0, "0", x)) +
+  scale_y_continuous(labels = function(y) ifelse(y == 0, "0", y)) +
+  xlab("Quantile") +
+  ylab(NULL) +
+  theme_bw(base_size = 11) +
+  theme(panel.grid.major = element_line(size = 0.05), 
+        panel.grid.minor = element_line(size = 0.05))
+
+# ggsave("figures/coverage_diff_states_2colors.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
+
