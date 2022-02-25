d44e6edc890cd37980c1fcc49b5ff5c27d53e77a
dwolffram <25206417+dwolffram@users.noreply.github.com>
2022-02-25 18:45:48 +0100

    clean up coverage plot

--- plot_coverage.R
+++ plot_coverage.R
@@ -12,117 +12,7 @@ coverage <- function(df){
   return(df)
 }
 
-plot_coverage <- function(df, date_column = target_end_date, B = 1000, difference = FALSE){
-  date_column <- enquo(date_column)
-  dates <- df %>% distinct(!!date_column) %>% pull()
-  
-  # compute coverage on all bootstrap samples
-  coverage_df = data.frame()  
-  for(i in 1:B){
-    dates_resampled <- sample(dates, replace = TRUE)
-    
-    # simple filter doesn't work because the same date can occur multiple times
-    df_resampled <- lapply(dates_resampled, function(x){df %>% filter(target_end_date == x)}) %>%
-      bind_rows()
-    
-    coverage_sample <- coverage(df_resampled)
-    
-    coverage_df <- bind_rows(coverage_df, coverage_sample)
-  }
-  
-  # compute CIs from bootstrapped coverage, we only use the lower (upper) bound for the lower (upper) value
-  results <- coverage_df %>%
-    group_by(model, quantile) %>%
-    summarize(l_5 = quantile(l, 0.05),
-              l_95 = quantile(l, 0.95),
-              l_25 = quantile(l, 0.25),
-              l_75 = quantile(l, 0.75),
-              u_5 = quantile(u, 0.05),
-              u_95 = quantile(u, 0.95),
-              u_25 = quantile(u, 0.25),
-              u_75 = quantile(u, 0.75), 
-              .groups = "drop")
-  
-  # compute coverage on full sample
-  coverage_full <- coverage(df)
-  
-  results <- results %>%
-    left_join(coverage_full, by = c("model", "quantile"))
-  
-  if (!difference){
-    g <- ggplot(results) +
-      facet_wrap("model", ncol = 3) +
-      geom_segment(aes(x = 0, xend = 1, y = 0, yend = 1), size = 0.2, linetype = "solid", colour = "grey70")+
-      geom_ribbon(aes(x = quantile, ymin = l_5, ymax = l_95), fill = "darkblue", alpha = 0.2) +
-      geom_ribbon(aes(x = quantile, ymin = u_5, ymax = u_95), fill = "darkred", alpha = 0.2) +
-      geom_errorbar(aes(x=quantile, ymin = l, ymax = u), width = 0.0125, size = 0.3, colour = "black") +
-      scale_x_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1),
-                         labels = function(x) ifelse(x == 0, "0", x)) +
-      scale_y_continuous(labels = function(y) ifelse(y == 0, "0", y)) +
-      xlab("Quantile") +
-      ylab(NULL) +
-      theme_bw(base_size = 11) +
-      theme(panel.grid.major = element_line(size = 0.05), 
-            panel.grid.minor = element_line(size = 0.05)) +
-      coord_fixed()
-    
-    print(g)
-    invisible(results)
-  }
-  else{
-    results_diff <- results %>% 
-      mutate_at(vars("l", "u", 
-                     "l_5", "l_25", "l_75", "l_95", 
-                     "u_5", "u_25","u_75", "u_95"), 
-                list(~ . - quantile))
-    
-    g <- ggplot(results_diff) +
-      facet_wrap("model") +
-      geom_hline(yintercept = 0, size = 0.3, linetype = "solid", color = "darkgray") +
-      geom_ribbon(aes(x = quantile, ymin = l_5, ymax = l_95), fill = "darkblue", alpha = 0.2) +
-      geom_ribbon(aes(x = quantile, ymin = u_5, ymax = u_95), fill = "darkred", alpha = 0.2) +
-      geom_errorbar(aes(x=quantile, ymin = l, ymax = u), width = 0.0125, size = 0.3, colour = "black") +
-      scale_x_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1),
-                         labels = function(x) ifelse(x == 0, "0", x)) +
-      scale_y_continuous(labels = function(y) ifelse(y == 0, "0", y)) +
-      xlab("Quantile") +
-      ylab(NULL) +
-      theme_bw(base_size = 11) +
-      theme(panel.grid.major = element_line(size = 0.05), 
-            panel.grid.minor = element_line(size = 0.05))
-    
-    print(g)
-    invisible(results_diff)
-  }
-}
-
-# Load data
-models <- c("KITmetricslab-select_ensemble", "COVIDhub-ensemble", "COVIDhub-baseline")
-
-# df <- read_csv("data/2022-01-03_df_processed.csv.gz", col_types = cols()) %>%
-#   filter(location != "US")
-
-df <- read_csv("data/2022-01-03_df_processed.csv.gz", col_types = cols()) %>%
-  filter(location_name == "Vermont")
-
-df <- df %>%
-  filter(target == "1 wk ahead inc death",
-         model %in% models) %>% 
-  select(- target)
-
-# Plot coverage
-results <- plot_coverage(df, B = 100)
-
-# ggsave("figures/coverage_national.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
-# ggsave("figures/coverage_states.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
-
-results_diff <- plot_coverage(df, B = 100, difference = TRUE)
-
-# ggsave("figures/coverage_diff_national.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
-# ggsave("figures/coverage_diff_states.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
-
-
-### CONSISTENCY BANDS
+# get critical value of binomial test (used for consistency bands)
 get_cr <- function(sample_size, alpha, nominal_level, alternative='less'){
   if(alternative == 'less'){
     C <- 0
@@ -136,6 +26,7 @@ get_cr <- function(sample_size, alpha, nominal_level, alternative='less'){
 }
 
 plot_coverage <- function(df, date_column = target_end_date, B = 1000, type = "confidence", difference = FALSE){
+  
   # some customizations used in all plots
   my_theme <- list(
     scale_x_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1), labels = function(x) ifelse(x == 0, "0", x)),
@@ -211,6 +102,7 @@ plot_coverage <- function(df, date_column = target_end_date, B = 1000, type = "c
         my_theme
     }
   }
+  
   else if (type == "consistency"){
     results <- coverage(df)
     
@@ -255,10 +147,11 @@ plot_coverage <- function(df, date_column = target_end_date, B = 1000, type = "c
       
     }
   }
-  
   print(g)
   invisible(results)
 }
+
+
 
 # Load data
 models <- c("KITmetricslab-select_ensemble", "COVIDhub-ensemble", "COVIDhub-baseline")
@@ -271,8 +164,11 @@ df <- read_csv("data/2022-01-03_df_processed.csv.gz", col_types = cols()) %>%
 
 df <- df %>%
   filter(target == "1 wk ahead inc death",
-         model %in% models) %>% 
-  select(- target)
+         model %in% models)
+
+# Plot coverage
+results <- plot_coverage(df, B = 100)
+results_diff <- plot_coverage(df, B = 100, difference = TRUE)
 
 plot_coverage(df, B=100, type="confidence", difference=FALSE)
 plot_coverage(df, B=100, type="confidence", difference=TRUE)
@@ -280,7 +176,8 @@ plot_coverage(df, B=100, type="consistency", difference=FALSE)
 plot_coverage(df, B=100, type="consistency", difference=TRUE)
 
 
-# ggsave("figures/coverage_diff_national_consistency.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
+# ggsave("figures/coverage_national_consistency.pdf", width=180, height=70, unit="mm", device = "pdf", dpi=300)
+
 
 
 
@@ -293,8 +190,7 @@ df <- read_csv("data/2022-01-03_df_processed.csv.gz", col_types = cols()) %>%
 
 df <- df %>%
   filter(target == "1 wk ahead inc death",
-         model %in% models) %>% 
-  select(- target)
+         model %in% models)
 
 coverage2 <- function(df){
   df$l <- df$truth < floor(df$value)
@@ -340,4 +236,3 @@ ggplot(coverage_df) +
   coord_fixed()
 
 # ggsave("figures/coverage_3states_consistency.pdf", width=200, height=200, unit="mm", device = "pdf", dpi=300)
-
